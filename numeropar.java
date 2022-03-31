147b3c407027670b6ee42ba775a636b4312df001
Rafael Archanjo <70230261+Rafael-Archanjo@users.noreply.github.com>
2022-03-31 08:52:54 -0300

    Update numeropar.java

--- numeropar.java
+++ numeropar.java
@@ -1,21 +1,27 @@
 import java.util.Scanner;
 
 public class numeropar {
-
 	public static void main(String[] args) {
+
 		Scanner ler = new Scanner(System.in);
-		int n,a;
-		
+		int n, a;
+
 		System.out.printf("Digite o numero: %n");
 		n = ler.nextInt();
-		
-		if(n % 2 == 0) {
-			System.out.printf("O numero � par");
+
+		if (n % 2 == 0) {
+			System.out.printf("O numero é par");
 		} else {
-			System.out.printf("O numero � impar");
+			System.out.printf("O numero é impar");
 		}
-
-
+		
+		/*
+		  if (n % 2 != 0) {
+		  System.out.printf("O numero é impar");
+			
+		} else {
+			System.out.printf("O numero é par");
+		}
+		 */
 	}
-
 }
