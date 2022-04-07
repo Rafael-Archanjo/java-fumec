30aec307ea3752c4130973a9a370c910a4017967
Rafael Archanjo <70230261+Rafael-Archanjo@users.noreply.github.com>
2022-04-07 08:48:23 -0300

    Update maiormenorzero.java

--- maiormenorzero.java
+++ maiormenorzero.java
@@ -6,30 +6,29 @@ public class maiormenorzero {
 		// TODO Auto-generated method stub
 		Scanner ler = new Scanner(System.in);
 		float x, y;
-		
-		System.out.printf("Digite o 1∫ num: %n");
+
+		System.out.printf("Digite o 1¬∫ num: %n");
 		x = ler.nextFloat();
-		
-		System.out.printf("Digite o 2∫ num: %n");
+
+		System.out.printf("Digite o 2¬∫ num: %n");
 		y = ler.nextFloat();
-		
+
 		if (x >= 0 && y >= 0) {
+
 			if (x == 0 || y == 0) {
-				System.out.printf("Um dos numeros È igual a zero");
+				System.out.printf("Um dos numeros √© igual a zero");
 			} else {
-			System.out.printf("Ambos numeros s„o maiores que zero");
+				System.out.printf("Ambos numeros s√£o maiores que zero");
 			}
-		}
-		if (x <= 0 && y <= 0) {
+		} else if (x <= 0 && y <= 0) {
+			
 			if (x == 0 || y == 0) {
-				System.out.printf("Um dos numeros È igual a zero");
+				System.out.printf("Um dos numeros √© igual a zero");
 			} else {
-			System.out.printf("Ambos numeros s„o menores que zero");
+				System.out.printf("Ambos numeros s√£o menores que zero");
 			}
-		}
-		
-		if (x > 0 && y < 0 || x < 0 && y > 0) {
-			System.out.printf("Um numero È maior e o outro menor que zero");
+		} else {
+			System.out.printf("Um numero √© maior e o outro menor que zero");
 		}
 	}
 
